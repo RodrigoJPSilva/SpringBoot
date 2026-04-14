f6c960c072107b86ddd2228d2a227b82424a1b29
Rodrigo Silva <rodrigojrpsilva@gmail.com>
2026-04-13 23:05:57 -0300

    Fix: change findBy ways

--- ToDoGraddle/ToDo/src/main/java/com/List/ToDo/repository/TaskRepository.java
+++ ToDoGraddle/ToDo/src/main/java/com/List/ToDo/repository/TaskRepository.java
@@ -3,6 +3,8 @@ package com.List.ToDo.repository;
 import com.List.ToDo.entities.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TaskRepository extends JpaRepository<Task, Long> {
+import java.util.List;
 
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByUserId(Long userId);
 }
